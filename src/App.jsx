980c6aab8aa917631133790c48553271f17cf4b7
shawlycode <kutinaorlando@gmail.com>
2024-08-15 14:05:31 +0000

    created login

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Header from "./components/header/Header"
 import { Hero } from "./components/header/hero/Hero"
 import { About } from "./pages/About"
+import { Login } from "./pages/login/Login"
 import { Pitches } from "./pages/pitches/Pitches"
 
 
@@ -11,9 +12,10 @@ function App() {
   return (
     <>
       <Header />
-      <Hero />
-      <About />
-      <Pitches />
+      {/* <Hero /> */}
+      {/* <About /> */}
+      {/* <Pitches /> */}
+      <Login />
     </>
   )
 }
