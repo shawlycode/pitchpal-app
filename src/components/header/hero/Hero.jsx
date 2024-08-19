420fe0b76f6f8266e605821c225ca82e91d5427e
shawlycode <kutinaorlando@gmail.com>
2024-08-19 08:00:07 +0000

    updated

--- src/components/header/hero/Hero.jsx
+++ src/components/header/hero/Hero.jsx
@@ -16,8 +16,8 @@ export function Hero() {
             Platform in Africa</h1>
           <p>Let's build it together</p>
           <button className='btn1'>Apply Now</button>
-          <div className="up_box2"><h3>Investors</h3></div>
-          <div className="up_box"><h3>Entrepreneurs</h3></div>
+          <div className="up_box2"><h3>Entrepreneurs</h3></div>
+          <div className="up_box"><h3>Investors</h3></div>
         </div>
         <div className="right-container"></div>
       </div>
